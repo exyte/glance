25174f5bab293cba4dd3825888ff94004d015879
Anton Salnik <skanton@gmail.com>
2017-04-20 21:51:17 +0700

    Update copyrights to Exyte.

--- plugins/com.xored.glance.ui.controls/src/com/xored/glance/ui/controls/tree/TreeItemContent.java
+++ plugins/com.xored.glance.ui.controls/src/com/xored/glance/ui/controls/tree/TreeItemContent.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2012 xored software, Inc.
+ * Copyright (c) 2017 Exyte
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     xored software, Inc. - initial API and implementation (Yuri Strot)
+ *     Yuri Strot - initial API and Implementation
  ******************************************************************************/
 package com.xored.glance.ui.controls.tree;
 
--- plugins/com.xored.glance.ui/src/com/xored/glance/internal/ui/search/SearchScopeEntry.java
+++ plugins/com.xored.glance.ui/src/com/xored/glance/internal/ui/search/SearchScopeEntry.java
@@ -1,5 +1,5 @@
 /******************************************************************************* 
- * Copyright (c) 2008 xored software, Inc.  
+ * Copyright (c) 2017 Exyte  
  * 
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Eclipse Public License v1.0 
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html  
  * 
  * Contributors: 
- *     xored software, Inc. - initial API and Implementation (Yuri Strot) 
+ *     Yuri Strot - initial API and Implementation 
  *******************************************************************************/
 package com.xored.glance.internal.ui.search;
 
