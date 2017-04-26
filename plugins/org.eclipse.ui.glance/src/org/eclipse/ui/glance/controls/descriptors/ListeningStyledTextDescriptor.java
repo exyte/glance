db340995e787142f5559aaa92eb5823e1886a631
Anton Salnik <skanton@gmail.com>
2017-04-26 19:53:19 +0700

    Add missing copyrights.

--- plugins/org.eclipse.ui.glance/src/org/eclipse/ui/glance/controls/descriptors/ListeningStyledTextDescriptor.java
+++ plugins/org.eclipse.ui.glance/src/org/eclipse/ui/glance/controls/descriptors/ListeningStyledTextDescriptor.java
@@ -1,6 +1,13 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2017 Exyte
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Yuri Strot - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ui.glance.controls.descriptors;
 
 import org.eclipse.swt.custom.StyledText;
