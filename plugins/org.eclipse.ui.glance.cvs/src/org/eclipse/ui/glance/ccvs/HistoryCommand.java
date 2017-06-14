50774aac1f01a97771279147fb93686271afde84
Yuri Strot <ystrot@gmail.com>
2017-06-14 17:52:31 +0700

    Update copyright headers for CVS support

--- plugins/org.eclipse.ui.glance.cvs/src/org/eclipse/ui/glance/ccvs/HistoryCommand.java
+++ plugins/org.eclipse.ui.glance.cvs/src/org/eclipse/ui/glance/ccvs/HistoryCommand.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2017 Exyte
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- *     Ivan Lobachev - initial API and implementation
- ******************************************************************************/
+ *     IBM Corporation - initial API and implementation
+ *     Ivan Lobachev - adapted to reuse from Glance
+ *******************************************************************************/
 package org.eclipse.ui.glance.ccvs;
 
 import java.util.ArrayList;
